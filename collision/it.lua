c9f2bab5caf84158a07e3b428cb4b606aca2b8d0
David L. Inman <davidlevere@gmail.com>
2020-10-11 01:00:24 -0400

    collision stub

---
+++ collision/it.lua
@@ -0,0 +1,15 @@
+
+-- bound, location, direction vector
+
+-- id of movee
+-- vector of the move
+-- return boolean, ids of colliders
+local function collides(move_id, vector)
+    
+end
+
+
+
+-- door
+-- wall
+-- letter
